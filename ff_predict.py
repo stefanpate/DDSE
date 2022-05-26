Fixed errors concerning mean centering and masking for SST work. Tried predicting SST with DMD. Updated all files to pull data from single directory /cnl/data/spate/Datasets. Made dictionary in library.py that maps strings to dataset filenames, which made ff net files more generalizable.

ff_predict.py
@@ -4,6 +4,7 @@ import numpy as np
 from ff_neural_net import Net
 import matplotlib.pyplot as plt
 import sys
+from library import data2fn
 
 '''
 Cmd line instructions:
@@ -31,9 +32,8 @@ gpu = int(sys.argv[2]) # GPU number. -1 for CPU
 n_steps = 4000 # Length of time series
 n_samples = 500 # Total number of data samples
 dt = 0.01
-data_dir = '/home/spate/Res/targets/'
-data_fn = data_dir + 'mackey_glass_beta_2_gamma_1_n_9.65_tau_2_n_samples_500_n_steps_4000_dt_0.01.csv'
-# data_fn = data_dir + 'lorenz_params_sig_10.00_rho_28.00_beta_2.67_n_samples_500_n_steps_4000_dt_0.01.csv'
+dataset = 'lorenz'
+data_fn = data2fn[dataset]
 
 # Training
 batch_size = 50
@@ -51,7 +51,7 @@ else:
 # Create net
 if do_load:
     print("Loading")
-    net = torch.load(model_dir + f"lorenz_predict_net_{model_no}.pth", map_location=device)
+    net = torch.load(model_dir + f"{dataset}_predict_net_{model_no}.pth", map_location=device)
 else:
     net = Net(n_inputs, n_outputs, hidden_widths, activation_fcn).to(device)
     print("Feedforward net created")
@@ -148,4 +148,4 @@ if do_test:
 
 if do_save:
     print(f"Saving model #{model_no}")
-    torch.save(net, model_dir + f"lorenz_predict_net_{model_no}.pth")
+    torch.save(net, model_dir + f"{dataset}_predict_net_{model_no}.pth")

ff_test_loop.py
@@ -4,16 +4,10 @@ import numpy as np
 from ff_neural_net import Net
 import matplotlib.pyplot as plt
 import sys
+from library import data2fn
 
 '''
-Cmd line instructions:
-
-1. Model number (model_no)
-2. GPU (gpu): -1 => cpu, 0 and above => gpu
-3-6. do_train, do_test, do_save, do_load. Booleans
-
-Example command:
-python ff_predict.py 7 0 true false true false
+Loads and tests saved models.
 '''
 # Model
 model_dir = '/cnl/data/spate/Corn/models/'
@@ -29,8 +23,8 @@ gpu = -1 # GPU number. -1 for CPU
 n_steps = 4000 # Length of time series
 n_samples = 500 # Total number of data samples
 dt = 0.01
-data_dir = '/home/spate/Res/targets/'
-data_fn = data_dir + 'lorenz_params_sig_10.00_rho_28.00_beta_2.67_n_samples_500_n_steps_4000_dt_0.01.csv'
+dataset = 'lorenz'
+data_fn = data2fn[dataset]
 train_frac = 0.8 # Train / test data split
 switch_t = 20 # When to switch between teacher forcing and output feedback
 switch_step = int(20 / dt) # Timestep associated with switch_t
@@ -66,7 +60,7 @@ test_output_traces = []
 save_last_t = []
 for model_no in model_numbers:
     print(f"Loading model #{model_no}")
-    net = torch.load(model_dir + f"lorenz_predict_net_{model_no}.pth", map_location=device)
+    net = torch.load(model_dir + f"{dataset}_predict_net_{model_no}.pth", map_location=device)
 
     output = []
     for i in range(n_steps - 1):
@@ -97,5 +91,5 @@ for model_no in model_numbers:
 save_last_t = np.vstack(save_last_t) # (n_models, n_test_samples)
 test_output_traces = np.vstack(test_output_traces) # (n_models * n_test_samples * n_inputs, n_steps - 1)
 
-np.savetxt(save_dir + f"ff_nn_predict_lorenz_test_output_traces_models_{model_numbers[0]}_{model_numbers[-1]}.csv", test_output_traces, delimiter=',')
-np.savetxt(save_dir + f"ff_nn_predict_lorenz_test_last_t_models_{model_numbers[0]}_{model_numbers[-1]}.csv", save_last_t, delimiter=',')
+np.savetxt(save_dir + f"ff_nn_predict_{dataset}_test_output_traces_models_{model_numbers[0]}_{model_numbers[-1]}.csv", test_output_traces, delimiter=',')
+np.savetxt(save_dir + f"ff_nn_predict_{dataset}_test_last_t_models_{model_numbers[0]}_{model_numbers[-1]}.csv", save_last_t, delimiter=',')

heatmap_movie.py
@@ -10,22 +10,27 @@ Args:
 
 import numpy as np
 import sys
-from library import make_movie
+from library import make_movie, mask_sst
 
-data_dir = '/cnl/data/spate/Corn/'
+data_dir = '/cnl/data/spate/Datasets/'
+save_dir = '/cnl/data/spate/Corn/'
 y, x = int(sys.argv[1]), int(sys.argv[2])
 k = int(sys.argv[3])
 files = sys.argv[4:]
-mask = np.loadtxt(data_dir + 'sst_land_mask.csv', delimiter=',').reshape(180, 360, 1) # For SST
+
+# Get mask for SST data
+mask = np.loadtxt(data_dir + 'sst_mask.csv', delimiter=',')
+mask = mask.astype(bool).reshape(180, 360, 1)
 
 for f in files:
     print("Loading file:", f)
     data = np.loadtxt(data_dir + f, delimiter=',') # Load data
-    data = data.reshape(y, x, -1)
-
-    if 'sst' in f: # Mask sea surface data
-        data *= mask
+    
+    if 'sst' in f:
+        data = mask_sst(data, mask) # Mask sea surface data, shape inherited from mask
+    else:
+        data = data.reshape(y, x, -1) # Use user inputted shape
     
     save_as = f.split('.')[0]
     print("Making movie:", f)
-    make_movie(data, k, data_dir + save_as, cmap='plasma', interval=50, a=6) # Make movie
+    make_movie(data, k, save_dir + save_as, cmap='plasma', interval=50, a=6) # Make movie

library.py
@@ -25,7 +25,6 @@ def rand_svd(x, r):
 
     return u, s, vt
 
-
 # Dynamic mode decomposition
 
 class dmd:
@@ -189,7 +188,6 @@ def get_forms(n, max_exp):
 
     return forms
 
-
 def get_count(form, d):
     '''
     Counts the number of polynomial terms of specified form
@@ -279,11 +277,9 @@ def get_terms(form, vars):
 
     return terms
 
-# Make animated heatmap plot
-
-def make_movie(data, k, loc, cmap='viridis', interval=20, a=5):
+def make_movie(data, k, loc, cmap='viridis', interval=50, a=5, vmin=None, vmax=None):
     '''
-    Makes a gif movie and saves to location 'loc'.
+    Makes a gif heatmap movie and saves to location 'loc'.
     Uses k timesteps from (y, x, t) data tensor.
     Optional args: cmap, interval, a (scales figure)
     '''
@@ -291,10 +287,14 @@ def make_movie(data, k, loc, cmap='viridis', interval=20, a=5):
 
     # Set up figure
     y, x, t = data.shape
+
+    if (vmin == None) | (vmax == None):
+        vmin, vmax = data.min(), data.max()
+    
     fig = plt.figure(figsize=(a * (x/y), a * (x/x)))
     ax = plt.axes(xlim=(0, x), ylim=(0, y))
 
-    cax = ax.pcolormesh(np.flipud(data[:-1, :-1, 0]), cmap=cmap)
+    cax = ax.pcolormesh(np.flipud(data[:-1, :-1, 0]), cmap=cmap, vmin=vmin, vmax=vmax)
     fig.colorbar(cax)
 
     def animate(i):
@@ -306,3 +306,30 @@ def make_movie(data, k, loc, cmap='viridis', interval=20, a=5):
     anim.save(loc + '.gif', writer='imagemagick')
 
     plt.close()
+
+def mask_sst(sst_st, mask):
+    '''
+    Returns a 3d masked array from provided
+    2d sst array (space, time), and boolean mask.
+    '''
+    lat_span, long_span = mask.shape[:2]
+    t_span = sst_st.shape[-1]
+    sst_masked = np.zeros(shape=(lat_span * long_span, t_span))
+    sst_masked[mask.reshape(-1,), :] = sst_st # Put real sst data in place
+    mask = np.repeat(mask, t_span, axis=-1)
+    sst_masked = sst_masked.reshape(lat_span, long_span, t_span)
+    sst_masked = np.ma.masked_where(~mask, sst_masked) # Return masked tensor for visualization
+
+    return sst_masked
+
+# Dict mapping dataset names to file names
+data_dir = '/cnl/data/spate/Datasets/'
+
+data2fn = {
+            'lorenz':data_dir + 'lorenz_params_sig_10.00_rho_28.00_beta_2.67_n_samples_500_n_steps_4000_dt_0.01.csv',
+            'mackey_glass':data_dir + 'mackey_glass_beta_2_gamma_1_n_9.65_tau_2_n_samples_500_n_steps_4000_dt_0.01.csv',
+            'rossler':data_dir + 'rossler_params_a_0.20_b_0.20_c_5.70_n_samples_500_n_steps_8000_dt_0.01.csv',
+            'sine':data_dir + 'sine_period_10_n_samples_100_n_steps_4000_dt_0.01.csv',
+            'sst':data_dir + 'sst.csv',
+            'sst_mask':data_dir + 'sst_mask.csv',
+            }